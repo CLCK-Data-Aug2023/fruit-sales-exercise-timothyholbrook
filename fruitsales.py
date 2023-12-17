5acfdf0b52ddeeec6b4a57ad527adbd08da694e0
Timothy Holbrook <timothy.holbrook03@gmail.com>
2023-12-17 14:42:29 -0500

    add fruit.csv

--- fruitsales.py
+++ fruitsales.py
@@ -1,1 +1,16 @@
-# add your code here
+import pandas as pd
+
+# Create a list
+data = [
+    {'Apples': 35, 'Bananas': 21},
+    {'Apples': 41, 'Bananas': 34}
+]
+
+# Create a pandas dataframe
+df = pd.DataFrame(data, index=['2017 Sales', '2018 Sales'])
+
+# Write to CSV file named fruit.csv
+df.to_csv('fruit.csv')
+
+# Display
+print(df)
